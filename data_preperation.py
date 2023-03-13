852dab40fdb50ea4712c0f7757f2ae69cc714e1e
Max Hahn <maxhahn93@outlook.de>
2023-03-13 15:23:01 +0100

    sparse data prerp

--- data_preperation.py
+++ data_preperation.py
@@ -1,15 +1,9 @@
+import os
 from utils import *
-
 from torch_geometric.datasets import TUDataset
-from torch_geometric.loader import DataLoader
-
-import neural_tangents as nt
-from neural_tangents import stax
-import jax
 from jax import numpy as jnp
-
-from jax._src.typing import Array, Shape
-from typing import Callable, Tuple, List
+from jax._src.typing import Array
+from typing import Tuple, List
 
 
 def ravel_multi_index_upper(a: Array, b: Array, a_dim: int, b_dim: int) -> Array:
@@ -60,6 +54,8 @@ def twl_sparse_pattern(edge_list: Array, nb_nodes: int) -> Tuple[Array, Array]:
     )
 
     # calculate the indices on the theoratical dense matrix
+    # where indices on the (theoretical) lower triangle are
+    # swapped with upper triangle indices
     a = ravel_multi_index_upper(
         edge_list_full[:, 0], edge_list_full[:, 1], nb_nodes, nb_nodes
     )
@@ -78,8 +74,8 @@ def twl_sparse_pattern(edge_list: Array, nb_nodes: int) -> Tuple[Array, Array]:
     pattern = jnp.array([a, b, c]).transpose()
 
     # sorting only for debuging
-    ind = jnp.lexsort((pattern[:, 1], pattern[:, 0]), 0)
-    pattern = pattern[ind, :]
+    # ind = jnp.lexsort((pattern[:, 1], pattern[:, 0]), 0)
+    # pattern = pattern[ind, :]
 
     return pattern, edge_list
 
@@ -95,208 +91,173 @@ def twl_sparse_edge_features(
     return edge_features
 
 
-def initial_edge_features(
-    graps_node_features, graphs_edge_features, edge_featur_init, nb_graphs, max_nodes
-):
-    """
-    get the initial edge feature matrix for the 2 WL
-    algorithm.
-    when the graph specification has only node features
-    as a batch x nodes x nodes x channels array
-    """
-    feature_dim = graps_node_features[0][0].shape[0]
-    # feature_dim = graps_node_features[0].shape[1]
-    print(f"create a array of shape", nb_graphs, max_nodes, max_nodes, feature_dim)
-    graphs_edge_features_from_nodes = jnp.zeros(
-        (nb_graphs, max_nodes, max_nodes, feature_dim)
-    )
-    for k, node_features in enumerate(graps_node_features):
-        for i, node_feature in enumerate(node_features):
-            graphs_edge_features_from_nodes = graphs_edge_features_from_nodes.at[
-                k, i, i, :
-            ].set(node_feature)
+def check_if_output_allready_exists(type: str, dataset_path: str) -> bool:
+    def check(files: List[str]) -> bool:
+        all_not_exist = True
+        for f in files:
+            all_not_exist = all_not_exist and not os.path.exists(dataset_path + f)
 
-    if graphs_edge_features == None:
+        return not all_not_exist
 
-        if edge_featur_init == "ONE_HOT":
-            graphs_edge_features = list()
-            for k, node_features in enumerate(graps_node_features):
-                nb_nodes = node_features.shape[0]
-                tmp = jnp.identity(nb_nodes**2)
-                tmp = jnp.reshape(tmp, (nb_nodes, nb_nodes, nb_nodes**2))
-                graphs_edge_features.append(
-                    zero_append(tmp, (max_nodes, max_nodes, max_nodes**2))
-                )
-                graphs_edge_features = jnp.array(graphs_edge_features)
-        if edge_featur_init == "BIAS":
-            graphs_edge_features = jnp.full(
-                graphs_edge_features_from_nodes.shape[:-1], 1
-            )
-            graphs_edge_features = jnp.expand_dims(graphs_edge_features, 3)
-        else:
-            raise Exception("Parameter edge_featur_init must be ONE_HOT or BIAS")
-
-    return jnp.append(graphs_edge_features_from_nodes, graphs_edge_features, axis=3)
-
-
-def diag(x, batched=True):
-    """
-    Arange a 2-dim arrary into a 3-dim array.
-    Where the 3-dim array has in the channel
-    dimension diagonal matricies filled
-    with the values from the 2-dim ijnput.
-    e.g
-    diag(jnp.array[[1,3],[3,4], [5,6]])
-    = [ [[1,0], [0,3]], [[3,0], [0,4]], [[5,0], [0,6]]]
-    """
-    if batched:
-        out = jnp.zeros((x.shape[0], x.shape[1], x.shape[1]))
-        for i in range(0, x.shape[1]):
-            out = out.at[:, i, i].set(x[:, i])
-    else:
-        out = jnp.zeros((x.shape[0], x.shape[0]))
-        out = out.at[jnp.diag_indices(out.shape[0])].set(x)
-    return out
-
-
-def calc_graph_conv_pattern(A, batched=True):
-    A_tilde = A + jnp.identity(A.shape[1])
-    A_tilde = A_tilde.at[A_tilde == 2].set(1)
-    if batched:
-        D_tilde = jnp.sum(A_tilde, axis=2)
-    else:
-        D_tilde = jnp.sum(A_tilde, axis=1)
-    D_tilde = 1 / jnp.sqrt(D_tilde)
-    D_tilde = diag(D_tilde, batched)
-    return D_tilde @ A_tilde @ D_tilde
-
-
-def expand_pattern_at_channels_dim(pattern_in, nr_channels, batched=True):
-    """
-    Expand a (batched) two dimensional pattern
-    into a three dimensional pattern. The size of the added
-    dimension is determined by nr_channels.
-    The channe
-    """
-
-    if batched:
-        out = jnp.zeros(
-            (
-                pattern_in.shape[0],
-                pattern_in.shape[1],
-                nr_channels,
-                pattern_in.shape[1],
-                nr_channels,
-            )
-        )
-        for k in range(pattern_in.shape[0]):
-            for i in range(nr_channels):
-                out = out.at[k, :, i, :, i].set(pattern_in[k, :])
-    else:
-        out = jnp.zeros(
-            (pattern_in.shape[1], nr_channels, pattern_in.shape[1], nr_channels)
-        )
-        for i in range(nr_channels):
-            out = out.at[:, i, :, i].set(pattern_in)
-    return out
-
-
-def calc_graph_conv_patterns(As):
-    """
-    calcualte the graph convolution pattern for each graph
-    """
-    patterns = list()
-    for A in As:
-        p = calc_graph_conv_pattern(A, False)
-        patterns.append(expand_pattern_at_channels_dim(p, 7, False))
-    patterns = jnp.array(patterns)
-    return patterns
-
-
-def pattern_preperation(edge_index, nb_graphs, max_nodes, two_wl_radius=[1]):
-    """
-    As: List,
-    """
-
-    max_edges = max([x.shape[1] for x in edge_index])
-
-    # need the adjacency matrix for the 2WL pattern
-    As = [to_dense(e, len(e)) for e in edge_index]
-    # unify the sizes of all adjacency matricies in the dataset, for the pattern callculation
-    As = [zero_append(a, (max_nodes, max_nodes)) for a in As]
-
-    # calculate the graph convolution pattern for each graph (dense pattern)
-    # graph_conv_pattern = calc_graph_conv_patterns(As)
-
-    print("Preparing GCN pattern")
-    # calculate the graph convolution pattern for each graph (sparse pattern)
-    edge_index = jnp.array([zero_append(x, (2, max_edges)) for x in edge_index])
-    graph_conv_pattern = jnp.swapaxes(edge_index, 1, 2)
-    graph_conv_pattern = jnp.expand_dims(graph_conv_pattern, 2)
-    graph_conv_pattern = jnp.array(graph_conv_pattern, dtype="int32")
-
-    print("Preparing 2WL pattern")
-    # calculate the 2 wl pattern (or patterns if multiple radia are given)
-    As = jnp.array(As)
-    two_wl_pattern = []
-    for radius in two_wl_radius:
-        As = r_power_adjacency_matrix(As, radius)  # does nothing, if r = 1
-        As_int = neigbourhood_intersections(As)
-        As_pattern = jnp.transpose(jnp.array(jnp.nonzero(As_int)))
-        two_wl_pattern.append(As_pattern)
-
-    return graph_conv_pattern, two_wl_pattern
-
-
-def feature_prepeation(
-    graps_node_features, graps_edge_features, edge_featur_init, nb_graphs, max_nodes
-):
-
-    print("Preparing edge features")
-    # update the edge features. If a graph has no edge features given, use a "one hot encoding" for the edges.
-    graps_edge_features = initial_edge_features(
-        graps_node_features, graps_edge_features, edge_featur_init, nb_graphs, max_nodes
-    )
-
-    print("Preparing node features")
-    # from a list of node features to a batched tensor of node features for the graph conv alg.
-    graps_node_features = [
-        zero_append(ef, (max_nodes, ef.shape[1])) for ef in graps_node_features
+    files_TWL = [
+        "/two_wl_edge_features.npy",
+        "/two_wl_patterns.npy",
+        "/two_wl_ys.npy",
+        "/two_wl_nb_edges.npy",
+        "/two_wl_patterns_graph_map.npy",
     ]
-    graps_node_features = jnp.expand_dims(jnp.array(graps_node_features), 3)
 
-    return graps_node_features, graps_edge_features
+    files_GCN = [
+        "/gcn_sparse_node_features",
+        "/gcn_sparse_patterns",
+        "/gcn_sparse_ys",
+        "/gcn_sparse_nb_nodes",
+        "/gcn_sparse_patterns_graph_map",
+    ]
+
+    if type == "TWL":
+        return check(files_TWL)
+    elif type == "GCN":
+        return check(files_GCN)
+    else:
+        raise Exception(
+            f"Type {type} not valid for check_if_output_allready_exists! Type must be one of TWL, GCN!"
+        )
 
 
-def data_preperation(
-    edge_index,
-    graps_node_features,
-    graps_edge_features,
-    edge_featur_init,
-    ys,
-    dataset_name,
-    base_path,
-    two_wl_radius,
+def prepare_tu_data_for_2WL(
+    tu_datasets: List[str], base_path: str, base_path_tu_datasets: str
 ):
-    """
-    As, graps_node_features, ys: Lists
-    dataset_name, base_path: Strings
-    two_wl_radius: List of ints
-    """
 
-    nb_graphs = len(graps_node_features)
-    max_nodes = len(max(graps_node_features, key=lambda x: len(x)))
+    for dataset_name in tu_datasets:
+        dataset_path = base_path + f"/{dataset_name}"
 
-    graps_node_features, graps_edge_features = feature_prepeation(
-        graps_node_features, graps_edge_features, edge_featur_init, nb_graphs, max_nodes
-    )
-    graph_conv_pattern, two_wl_pattern = pattern_preperation(
-        edge_index, nb_graphs, max_nodes, two_wl_radius
-    )
+        print(f"Preparing {dataset_name} dataset:")
+        if check_if_output_allready_exists("TWL", dataset_path):
+            print(
+                f"All expected output files already exist at {dataset_path}. If you want to preproces again delete the files at that location. Skip the Dataset for now."
+            )
+            continue
 
-    jnp.save(base_path + f"/ys", ys)
-    jnp.save(base_path + f"/graps_node_features", graps_node_features)
-    jnp.save(base_path + f"/graphs_edge_features", graps_edge_features)
-    jnp.save(base_path + f"/graph_conv_pattern", graph_conv_pattern)
-    for r, p in zip(two_wl_radius, two_wl_pattern):
-        jnp.save(base_path + f"/two_wl_pattern_radius_{r}", p)
+        dataset = TUDataset(root=base_path_tu_datasets, name=dataset_name)
+
+        dataset_edge_features = list()
+        dataset_patterns = list()
+        dataset_ys = list()
+        dataset_nb_edges = list()
+        patterns_graph_map = list()
+        prev_pattern_graph_map = 0
+        for i, data in enumerate(dataset):
+            if i % 10 == 0:
+                print(f"Working on sample {i} to {i + 10} of {len(dataset)} samples.")
+
+            # calculate the edge_list and pattern array for each graph in the dataset
+            edge_list = np.transpose(np.array(data.edge_index))
+            node_features = np.array(data.x)
+            nb_nodes = len(node_features)
+            pattern, edge_list = twl_sparse_pattern(edge_list, nb_nodes)
+            edge_features = twl_sparse_edge_features(
+                node_features, edge_list.shape[0], nb_nodes
+            )
+
+            dataset_edge_features.append(edge_features)
+            dataset_patterns.append(pattern)
+            dataset_ys.append(np.array(data.y))
+            dataset_nb_edges.append(edge_list.shape[0])
+            patterns_graph_map.append(pattern.shape[0] + prev_pattern_graph_map)
+            prev_pattern_graph_map = patterns_graph_map[-1]
+
+        # merge all patterns and all edge lists into one big pattern
+        # and move the indices in pattern acordingly
+        pattern = dataset_patterns[0]
+        nb_edges_cum = dataset_nb_edges[0]
+        for current_nb_edges, current_pattern in zip(
+            dataset_nb_edges[1:], dataset_patterns[1:]
+        ):
+            pattern = np.append(pattern, current_pattern + nb_edges_cum, 0)
+            nb_edges_cum += current_nb_edges
+
+        edge_features = dataset_edge_features[0]
+        for current_edge_features in dataset_edge_features[1:]:
+            edge_features = np.append(edge_features, current_edge_features, 0)
+
+        if not os.path.exists(dataset_path):
+            os.makedirs(dataset_path)
+
+        np.save(dataset_path + f"/two_wl_edge_features", edge_features)
+        np.save(dataset_path + f"/two_wl_patterns", pattern)
+        np.save(dataset_path + f"/two_wl_ys", np.array(dataset_ys))
+        np.save(dataset_path + f"/two_wl_nb_edges", np.array(dataset_nb_edges))
+        np.save(
+            dataset_path + f"/two_wl_patterns_graph_map", np.array(patterns_graph_map)
+        )
+
+
+def prepare_tu_data_for_GCN(
+    tu_datasets: List[str], base_path: str, base_path_tu_datasets: str
+):
+
+    for dataset_name in tu_datasets:
+
+        dataset = TUDataset(root=base_path_tu_datasets, name=dataset_name)
+        dataset_path = base_path + f"/{dataset_name}"
+        if check_if_output_allready_exists("GCN", dataset_path):
+            print(
+                f"All expected output files already exist at {dataset_path}. If you want to preproces again delete the files at that location. Skip the Dataset for now."
+            )
+            continue
+
+        dataset_node_features = list()
+        dataset_patterns = list()
+        dataset_ys = list()
+        dataset_nb_nodes = list()
+        patterns_graph_map = list()
+        prev_pattern_graph_map = 0
+        for i, data in enumerate(dataset):
+            if i % 10 == 0:
+                print(f"Working on sample {i} to {i + 10} of {len(dataset)} samples.")
+
+            edge_list = np.transpose(np.array(data.edge_index))
+            node_features = np.array(data.x)
+            nb_nodes = len(node_features)
+
+            dataset_node_features.append(node_features)
+            dataset_patterns.append(edge_list)
+            dataset_ys.append(np.array(data.y))
+            dataset_nb_nodes.append(nb_nodes)
+            patterns_graph_map.append(node_features.shape[0] + prev_pattern_graph_map)
+            prev_pattern_graph_map = patterns_graph_map[-1]
+
+        # merge all patterns and all edge lists into one big pattern
+        # and move the indices in pattern acordingly
+        pattern = dataset_patterns[0]
+        nb_nodes_cum = dataset_nb_nodes[0]
+        for current_nb_nodes, current_pattern in zip(
+            dataset_nb_nodes[1:], dataset_patterns[1:]
+        ):
+            pattern = np.append(pattern, current_pattern + nb_nodes_cum, 0)
+            nb_nodes_cum += current_nb_nodes
+
+        node_features = dataset_node_features[0]
+        for current_node_features in dataset_node_features[1:]:
+            node_features = np.append(node_features, current_node_features, 0)
+
+        # node feature
+        node_features = np.expand_dims(node_features, 0)
+        node_features = np.expand_dims(node_features, 2)
+
+        # pattern
+        pattern = np.expand_dims(pattern, 0)
+        pattern = np.expand_dims(pattern, 2)
+
+        if not os.path.exists(dataset_path):
+            os.makedirs(dataset_path)
+
+        np.save(dataset_path + f"/gcn_sparse_node_features", node_features)
+        np.save(dataset_path + f"/gcn_sparse_patterns", pattern)
+        np.save(dataset_path + f"/gcn_sparse_ys", np.array(dataset_ys))
+        np.save(dataset_path + f"/gcn_sparse_nb_nodes", np.array(dataset_nb_nodes))
+        np.save(
+            dataset_path + f"/gcn_sparse_patterns_graph_map",
+            np.array(patterns_graph_map),
+        )
