c35ac545dd4743c49aaa9f75c1412aded4112101
Max Hahn <maxhahn93@outlook.de>
2023-03-13 18:18:03 +0100

    run

--- run.sh
+++ run.sh
@@ -2,6 +2,7 @@
 #
 #SBATCH -p All
 #SBATCH -c 1 # number of cores
+pip install jaxlib
 pip install jax
 pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
 pip install pyg-lib torch-scatter torch-sparse torch-cluster torch-spline-conv torch-geometric -f https://data.pyg.org/whl/torch-1.13.0+cpu.html
