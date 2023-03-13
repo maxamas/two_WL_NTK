3c8a97de5ac91b10b05caffcc348006d4cce1c13
Max Hahn <maxhahn93@outlook.de>
2023-03-13 18:12:26 +0100

    run

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-#SBATCH -p Al
+#SBATCH -p All
 #SBATCH -c 1 # number of cores
 pip install jax
 pip install torch torchvision torchaudio --extra-index-url https://download.pytorch.org/whl/cpu
