61283f81bafd8fed9ed592e67859e86a0a10ed3c
Max Hahn <maxhahn93@outlook.de>
2023-03-23 13:40:22 +0000

    add exception to kernel calculation

--- calculate_NTK_gram_matrix.py
+++ calculate_NTK_gram_matrix.py
@@ -12,13 +12,13 @@ def twl_kernel_function(kernel_fn, type="ntk"):
     # x and y are tuples with:
     # edge_features, ref_matrix, edge_features_graph_indx, ref_matrix_graph_indx, ys, nb_edges, nb_graphs, id_high
     return lambda x, y: kernel_fn(
-        x[0],
-        y[0],
+        x["edge_features"],
+        y["edge_features"],
         type,
-        pattern=(x[1], y[1]),
-        nb_edges=(x[0].shape[0], y[0].shape[0]),
-        nb_graphs=(x[6], y[6]),
-        graph_indx=(x[2], y[2]),
+        pattern=(x["ref_matrix"], y["ref_matrix"]),
+        nb_edges=(x["edge_features"].shape[0], y["edge_features"].shape[0]),
+        nb_graphs=(x["nb_graphs"], y["nb_graphs"]),
+        graph_indx=(x["edge_features_graph_indx"], y["edge_features_graph_indx"]),
     )
 
 
@@ -26,13 +26,13 @@ def gcn_kernel_function(kernel_fn, type="ntk"):
     # x and y are tuples with:
     # (node_features, edge_list, node_features_graph_indx, edge_list_graph_indx, ys, nb_nodes, nb_graphs, id_high)
     return lambda x, y: kernel_fn(
-        x[0],
-        y[0],
+        x["node_features"],
+        y["node_features"],
         type,
-        pattern=(x[1], y[1]),
-        nb_nodes=(x[0].shape[0], y[0].shape[0]),
-        nb_graphs=(x[6], y[6]),
-        graph_indx=(x[2], y[2]),
+        pattern=(x["edge_list"], y["edge_list"]),
+        nb_nodes=(x["node_features"].shape[0], y["node_features"].shape[0]),
+        nb_graphs=(x["nb_graphs"], y["nb_graphs"]),
+        graph_indx=(x["node_features_graph_indx"], y["node_features_graph_indx"]),
     )
 
 
@@ -44,12 +44,23 @@ def save_gram_matrix_batch_wise(
             kernel_matrix = kernel_fn(arrays_1, arrays_2)
             if not os.path.exists(kernel_path):
                 os.makedirs(kernel_path)
-            jnp.save(kernel_path + f"/NTK_{arrays_1[-1]}_{arrays_2[-1]}", kernel_matrix)
-        jnp.save(kernel_path + f"/Ys_2_{arrays_1[-1]}", arrays_1[4])
+            jnp.save(
+                kernel_path + f"/NTK_{arrays_1['id_high']}_{arrays_2['id_high']}",
+                kernel_matrix,
+            )
+        jnp.save(kernel_path + f"/Ys_2_{arrays_1['id_high']}", arrays_1["id_high"])
 
 
-def save_kernels(datasets_names: List[str], nn_types: List[str]):
+def save_kernels(path: str):
+
+    datasets_names = os.listdir(path)
+    datasets_names = [i for i in datasets_names if i in config.dataset_names]
+
     for dataset_name in datasets_names:
+
+        nn_types = os.listdir(path + "/" + dataset_name)
+        nn_types = [i for i in nn_types if i in config.nn_types]
+
         for nn_type in nn_types:
 
             base_path_preprocessed = (
@@ -57,42 +68,51 @@ def save_kernels(datasets_names: List[str], nn_types: List[str]):
             )
             kernel_path = config.kernel_base_path + f"/{dataset_name}/{nn_type}"
 
-            if nn_type == "GCN":
-                data_loader = GCN_Dataloader(
-                    file_path=base_path_preprocessed, nb_train_samples=160
-                )
-                _, _, kernel_fn = get_gcn_network_configuration(
-                    layers=10,
-                    parameterization="ntk",
-                    layer_wide=10,
-                    output_layer_wide=1,
-                )
-                decorated_kernel_fn = gcn_kernel_function(kernel_fn, type="ntk")
-            elif nn_type == "TWL":
-                data_loader = TWL_Dataloader(
-                    file_path=base_path_preprocessed, nb_train_samples=160
-                )
-                # layer with must be specified as int, but is ignored
-                _, _, kernel_fn = get_2wl_network_configuration(
-                    layers=3, parameterization="ntk", layer_wide=10, output_layer_wide=1
-                )
-                decorated_kernel_fn = twl_kernel_function(kernel_fn, type="ntk")
-            else:
-                print(f"No path for nn_type {nn_type}")
-                exit()
+            print(f"calculate kernel matrix for {nn_type} dataset {dataset_name}!")
 
-            # create a batch iterator for all samples yielded in an ordered way
-            data_it_1 = lambda: data_loader.batch_iterator(5, True)
-            data_it_2 = lambda: data_loader.batch_iterator(5, True)
+            try:
 
-            save_gram_matrix_batch_wise(
-                data_it_1, data_it_2, decorated_kernel_fn, kernel_path
-            )
+                if nn_type == "GCN":
+                    data_loader = GCN_Dataloader(
+                        file_path=base_path_preprocessed, nb_train_samples=160
+                    )
+                    _, _, kernel_fn = get_gcn_network_configuration(
+                        layers=10,
+                        parameterization="ntk",
+                        layer_wide=10,
+                        output_layer_wide=1,
+                    )
+                    decorated_kernel_fn = gcn_kernel_function(kernel_fn, type="ntk")
+                elif nn_type == "TWL":
+                    data_loader = TWL_Dataloader(
+                        file_path=base_path_preprocessed, nb_train_samples=160
+                    )
+                    # layer with must be specified as int, but is ignored
+                    _, _, kernel_fn = get_2wl_network_configuration(
+                        layers=3,
+                        parameterization="ntk",
+                        layer_wide=10,
+                        output_layer_wide=1,
+                    )
+                    decorated_kernel_fn = twl_kernel_function(kernel_fn, type="ntk")
+                else:
+                    print(f"No path for nn_type {nn_type}")
+                    exit()
+
+                # create a batch iterator for all samples yielded in an ordered way
+                data_it_1 = lambda: data_loader.batch_iterator(5, True)
+                data_it_2 = lambda: data_loader.batch_iterator(5, True)
+
+                save_gram_matrix_batch_wise(
+                    data_it_1, data_it_2, decorated_kernel_fn, kernel_path
+                )
+
+            except Exception as e:
+                print(f"Kernel matrix calculation failed with error {e}!")
 
 
 if __name__ == "__main__":
 
-    datasets_names = config.dataset_names
     nn_types = config.nn_types
 
-    save_kernels(datasets_names, nn_types)
+    save_kernels(config.dataloader_base_path)
--- config.py
+++ config.py
@@ -6,8 +6,48 @@ dataset_names = [
     "PROTEINS",
     "PTC",
     "NCI1",
-    "COLORS-3",  # "COLLAB", is to big
+    "COLORS-3",
     "IMDB-BINARY",
     "IMDB-MULTI",
 ]
 nn_types = ["TWL", "GCN"]
+
+gcn_gd_hyperparameters = {
+    "MUTAG": {
+        "learning_rate": 0.01,
+        "epochs": 100,
+        "layers": 3,
+        "parameterization": "standard",
+        "layer_wide": 32,
+        "output_layer_wide": 1,
+        "batch_size": 64,
+        "val_set_portion": 0.2,
+        "cv_folds": 10,
+    },
+    "PROTEINS": {},
+    "PTC": {},
+    "NCI1": {},
+    "COLORS-3": {},
+    "IMDB-BINARY": {},
+    "IMDB-MULTI": {},
+}
+
+twl_gd_hyperparameters = {
+    "MUTAG": {
+        "learning_rate": 0.01,
+        "epochs": 1,
+        "layers": 3,
+        "parameterization": "standard",
+        "layer_wide": 32,
+        "output_layer_wide": 1,
+        "batch_size": 64,
+        "val_set_portion": 0.2,
+        "cv_folds": 2,
+    },
+    "PROTEINS": {},
+    "PTC": {},
+    "NCI1": {},
+    "COLORS-3": {},
+    "IMDB-BINARY": {},
+    "IMDB-MULTI": {},
+}
